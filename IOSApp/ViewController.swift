e8767e8daa8defb49c62924a3312235f0412d2a6
Aaron <aarondsouza42@gmail.com>
2025-02-05 19:16:34 -0800

    Initial Commit

---
+++ IOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSApp
+//
+//  Created by Aaron D on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOSAppTests/IOSAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAppTests.swift
+//  IOSAppTests
+//
+//  Created by Aaron D on 2/5/25.
+//
+
+import Testing
+@testable import IOSApp
+
+struct IOSAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
