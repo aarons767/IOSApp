8f72da9a901b82a24fdc14563642db99e85a3e4b
Aaron <aarondsouza42@gmail.com>
2025-02-07 12:37:37 -0800

    Setup project starters

--- IOSApp/ViewController.swift
+++ IOSApp/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    var something = Int(0)
+
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        let name = textField.text ?? ""
+        label.text = "Hello, \(name)"
+
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textField.becomeFirstResponder()
     }
 
 
